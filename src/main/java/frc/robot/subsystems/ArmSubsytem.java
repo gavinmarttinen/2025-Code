8ca29022d6ddaca2f5840e32e10e4e5f43bbf450
Gunnar Marttinen <26539@students.dc.k12.mn.us>
2025-04-03 21:19:51 -0500

    Final push for Thursday - Vertical Pos in Auto

--- src/main/java/frc/robot/subsystems/ArmSubsytem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsytem.java
@@ -15,7 +15,7 @@ public class ArmSubsytem extends SubsystemBase{
 public ArmSubsytem() {
     pidController.enableContinuousInput(0, 1);
    armMotor.setInverted(false);
-   pidController.setTolerance(0);
+   pidController.setTolerance(0.0);
     
 }
 
